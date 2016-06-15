4481e22a8f9edeb4bce6624580d5cd8483bc0cce
ZhilkoRoman <roman.zhilko@gmail.com>
2016-06-15 18:02:46 +0300

    Activity вместо AppCompat

--- app/src/main/java/com/gr2/a2016/ease_l/MainActivity.java
+++ app/src/main/java/com/gr2/a2016/ease_l/MainActivity.java
@@ -1,9 +1,10 @@
 package com.gr2.a2016.ease_l;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
