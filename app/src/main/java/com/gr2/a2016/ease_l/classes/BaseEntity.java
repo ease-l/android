79c3f53e2200977cd98152ccb406fa7a40ee9b16
ShavelV <valshavel@gmail.com>
2016-06-16 18:04:53 +0300

    BaseEntity changed

--- app/src/main/java/com/gr2/a2016/ease_l/classes/BaseEntity.java
+++ app/src/main/java/com/gr2/a2016/ease_l/classes/BaseEntity.java
@@ -9,8 +9,47 @@ import java.util.Date;
  */
 
 public class BaseEntity {
-    public ObjectId id;
-    public Person author;
-    public Date creationDate;
-    public String version;
+    private ObjectId id;
+    private Person author;
+    private Date creationDate;
+    private String version;
+
+    public BaseEntity(Person author,ObjectId id,Date creationDate,String version) {
+        this.author = author;
+        this.id = id;
+        this.creationDate = creationDate;
+        this.version = version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public Person getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Person author) {
+        this.author = author;
+    }
 }
