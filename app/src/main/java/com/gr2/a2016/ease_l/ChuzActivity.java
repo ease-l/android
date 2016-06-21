9e8ff2d2fa7a434c5ee69457344a746ac154a839
awds333 <54825@mail.ru>
2016-06-21 14:42:58 -0200

    сделал адаптер

--- app/src/main/java/com/gr2/a2016/ease_l/ChuzActivity.java
+++ app/src/main/java/com/gr2/a2016/ease_l/ChuzActivity.java
@@ -1,13 +1,19 @@
 package com.gr2.a2016.ease_l;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
+import com.android.volley.RequestQueue;
+
 public class ChuzActivity extends Activity {
+    RequestQueue requestQueue;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chuz);
+        context = this;
     }
 }
