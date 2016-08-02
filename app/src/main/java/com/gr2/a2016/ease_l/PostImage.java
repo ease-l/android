405b00f780561131d3595b8db7a49bcc8051c7cd
ZhilkoRoman <roman.zhilko@gmail.com>
2016-08-02 16:25:52 +0300

    Что-то

---
+++ app/src/main/java/com/gr2/a2016/ease_l/PostImage.java
@@ -0,0 +1,14 @@
+package com.gr2.a2016.ease_l;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_image);
+
+    }
+}
