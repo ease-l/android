45035bd3fa663f0039494cde1b6aff3a2241804d
ZhilkoRoman <roman.zhilko@gmail.com>
2016-08-09 16:28:20 +0300

    NetworkAdresses

--- app/src/main/java/com/gr2/a2016/ease_l/classes/NetworkAdreses.java
+++ app/src/main/java/com/gr2/a2016/ease_l/classes/NetworkAdreses.java
@@ -5,8 +5,8 @@ package com.gr2.a2016.ease_l.classes;
  */
 public class NetworkAdreses {
     public static final String GET_ALL_PROJECTS = "http://ease-l.xyz/project";
-    public static final String GET_PROJECT_BY_ID = "http://ease-l.xyz/project/id";
-    public static final String GET_IMAGE_BY_ID = "http://ease-l.xyz/Image/id";
-    public static final String GET_COMMENT = "http://ease-l.xyz/Comment/id";
+    public static final String GET_PROJECT_BY_ID = "http://ease-l.xyz/project/";
+    public static final String GET_IMAGE_BY_ID = "http://ease-l.xyz/Image/";
+    public static final String GET_COMMENT = "http://ease-l.xyz/Comment/";
     public static final String POST_IMAGE = "http://ease-l.xyz/Image/Download";
 }
