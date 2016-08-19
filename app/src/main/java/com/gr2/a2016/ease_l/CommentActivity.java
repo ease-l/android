82a496568760526a3947eb3d55324dbc84ee29b7
awds333 <54825@mail.ru>
2016-08-19 19:33:11 -0200

    Получение картинки и коментов

--- app/src/main/java/com/gr2/a2016/ease_l/CommentActivity.java
+++ app/src/main/java/com/gr2/a2016/ease_l/CommentActivity.java
@@ -10,6 +10,5 @@ ImageView imageView;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coment);
-        imageView =(ImageView)findViewById(R.id.imageView3);
     }
 }
