6dc7bde9e6f44ad3614daa60ba3567640c2d6a87
awds333 <54825@mail.ru>
2016-08-19 12:31:44 -0200

    добавил

---
+++ app/src/main/java/com/gr2/a2016/ease_l/ComentActivity.java
@@ -0,0 +1,15 @@
+package com.gr2.a2016.ease_l;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+public class ComentActivity extends AppCompatActivity {
+ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coment);
+        imageView =(ImageView)findViewById(R.id.imageView3);
+    }
+}
